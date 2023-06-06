cc14b2c5d111d85568b81bc65578391febbc7584
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-07 02:23:36 +0300

    Add create, update, delete users

--- users_edit_api/routes/users.py
+++ users_edit_api/routes/users.py
@@ -9,23 +9,56 @@ router = APIRouter()
 @router.get('/users/')
 async def get_users():
     query = """
-        SELECT * FROM users
+        SELECT * FROM users;
     """
-    with get_connection() as conn:
-        users = await conn.fetchall(query)
-        return users
+    async with get_connection() as conn:
+        users = await conn.fetch(query)
+    return users
 
 
 @router.post('/users/')
-async def create_user():
-    return {}
+async def create_user(args: UserCreateOrUpdate):
+    query = """
+        INSERT INTO users (name, password, "group")
+        VALUES ($1, $2, $3)
+        RETURNING id;
+    """
+    async with get_connection() as conn:
+        users = await conn.fetch(
+            query,
+            args.name,
+            args.password,
+            args.group
+        )
+    return users
 
 
 @router.patch('/users/{user_id}')
 async def update_users(user_id: int, args: UserCreateOrUpdate):
-    return {}
+    query = """
+        UPDATE users SET name=$2, password=$3, "group"=$4
+        WHERE users.id = $1;
+    """
+    async with get_connection() as conn:
+        users = await conn.fetch(
+            query,
+            user_id,
+            args.name,
+            args.password,
+            args.group,
+        )
+    return users
 
 
-@router.post('/users/{user_id}')
+@router.delete('/users/{user_id}')
 async def delete_users(user_id: int):
-    return
+    query = """
+        DELETE FROM users
+        WHERE users.id = $1;
+    """
+    async with get_connection() as conn:
+        users = await conn.fetch(
+            query,
+            user_id,
+        )
+    return users
